faf92d2dca0e391b2b334e70af3720b9b6e940c5
Shivamprajapati844505 <sp1172389@gmail.com>
2025-02-15 02:50:18 +0530

    complete project

---
+++ public/js/map.js
@@ -0,0 +1,20 @@
+let map;
+
+async function initMap() {
+  const position = { lat:28.209, lng: 77.209 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 9,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "This is Your Position.",
+  });
+}
+initMap();
